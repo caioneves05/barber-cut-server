7801d41ae3dc460a66929f4de436930f21a3b3b4
caioneves05 <ncaio037@gmail.com>
2025-08-10 00:23:03 -0300

    fix: new spring structure

---
+++ src/test/java/com/barber_cut_server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barber_cut_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
